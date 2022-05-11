a94cd99b70c1a4ba772e48dbc31d05969eb002e9
GALAYA <shalendrakavishs205@gmail.com>
2022-05-11 18:07:26 +0530

    final changes

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import {initializeApp} from "firebase/app";
+
+export const firebaseApp = initializeApp({
+        apiKey: "AIzaSyAxq2V2t7byNVMJbAIYnWy8ZH5ucgt67ZQ",
+        authDomain: "sliit-y3s1-project.firebaseapp.com",
+        projectId: "sliit-y3s1-project",
+        storageBucket: "sliit-y3s1-project.appspot.com",
+        messagingSenderId: "653474595240",
+        appId: "1:653474595240:web:5fb2dfad992840255d1d9d",
+        measurementId: "G-3E6SFDFDLJ"
+});
